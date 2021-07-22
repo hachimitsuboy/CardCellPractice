5a2a2ff1aa9f3322d0798d5a89a1b384e8a9dc9f
hachimitsuboy <39763423+hachimitsuboy@users.noreply.github.com>
2021-07-22 15:53:07 +0900

    レクチャー２までの内容

--- Controller/HomeViewController.swift
+++ Controller/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
         view1.backgroundColor = .red
         let view2 = UIView()
         view2.backgroundColor = .yellow
-        let view3 = UIView()
-        view3.backgroundColor = .blue
+        let view3 = BottomControllView()
+
         
         //view用の配列的な扱いを今回しているstackViewさん
         let stackView = UIStackView(arrangedSubviews: [view1,view2,view3])
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
         
         [
             view1.heightAnchor.constraint(equalToConstant: 100),
-            view3.heightAnchor.constraint(equalToConstant: 100),
+            view3.heightAnchor.constraint(equalToConstant: 120),
             
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
